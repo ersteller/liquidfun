875f72f485479159eccfe798b86ded169deb84ea
ersteller <siggi2017@freenet.de>
2019-12-05 03:22:22 +0100

    refine testbed

--- liquidfun/Box2D/lfjs/testbed/tests/testMutualForce.js
+++ liquidfun/Box2D/lfjs/testbed/tests/testMutualForce.js
@@ -1,33 +1,49 @@
 function TestMutualForce() {
     camera.position.y = 2;
-    camera.position.z = 3;
+    camera.position.z = 6;
+
     var bodyDef = new b2BodyDef();
     var ground = world.CreateBody(bodyDef);
-  
+
+    this.g = new b2Vec2(0.0, -1.0);
+    this.og = new b2Vec2(0.0, 0.0);
+    this.currg = this.og;
+    world.SetGravity(this.currg);
+
+    /* create fence */
     var chainShape = new b2ChainShape();
-    chainShape.vertices.push(new b2Vec2(-2, 0));
-    chainShape.vertices.push(new b2Vec2(2, 0));
-    chainShape.vertices.push(new b2Vec2(2, 4));
-    chainShape.vertices.push(new b2Vec2(-2, 4));
+    chainShape.vertices.push(new b2Vec2(-2-2, 0-2));
+    chainShape.vertices.push(new b2Vec2(2+2, 0-2));
+    chainShape.vertices.push(new b2Vec2(2+2, 4+2));
+    chainShape.vertices.push(new b2Vec2(-2-2, 4+2));
   
     chainShape.CreateLoop();
     ground.CreateFixtureFromShape(chainShape, 0);
-  
-    var shape = new b2PolygonShape;
-    shape.SetAsBoxXYCenterAngle(0.8, 1, new b2Vec2(-1.2, 1.01), 0);
-  
+
+    /* particel properties */
     var psd = new b2ParticleSystemDef();
     psd.radius = 0.025;
     psd.dampingStrength = 0.2;
-  
     var particleSystem = world.CreateParticleSystem(psd);
-  
     var pd = new b2ParticleGroupDef();
+
+    /* shared linear velocity */
+    var dt = 0.27;
+
+    /* large body blob bottom left right move up */
+    var shape = new b2PolygonShape;
+    shape.SetAsBoxXYCenterAngle(0.8, 0.8, new b2Vec2(-0.0, 2.0), 0);
     pd.shape = shape;
+    pd.linearVelocity = new b2Vec2(0.00e+00, dt);
     var group = particleSystem.CreateParticleGroup(pd);
 
-    var og = new b2Vec2(0.000000000000000e+00, 0.000000000000000e+01);
-    world.SetGravity(og);
+    /* second body 8 times smaller blob top right move down */
+    var shape2 = new b2PolygonShape;
+    shape2.SetAsBoxXYCenterAngle(0.2, 0.4, new b2Vec2(1.8, 2.0), 0);
+    pd.shape = shape2;
+    pd.linearVelocity = new b2Vec2(0.00e+00, -dt * 8);
+    var group2 = particleSystem.CreateParticleGroup(pd);
+
 
     //add it to the mutual force contorller 
     console.log("init mfc is called");
@@ -52,13 +68,14 @@ TestMutualForce.prototype.Keyboard = function(char) {
 
         break;
       case 'g':
-        var g = new b2Vec2(0.000000000000000e+00, -1.000000000000000e+01);
-        var og = new b2Vec2(0.000000000000000e+00, 0.000000000000000e+01);
-        if (1)// world.GetGravity() == og) {
+
+        if (this.currg == this.og) // world.GetGravity() == og) { not implemented
         {
-            world.SetGravity(g);
+            world.SetGravity(this.g);
+            this.currg = this.g;
         } else {
-            world.SetGravity(og);
+            world.SetGravity(this.og);
+            this.currg = this.og
         }
 
         break;
